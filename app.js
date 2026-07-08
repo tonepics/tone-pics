248f935f24c4f330484c0d5880534b47ea6d3b83
tonepics <youseftob47@gmail.com>
2026-07-08 15:28:03 +0300

    Add main JavaScript file for Tone.pics

---
+++ app.js
@@ -0,0 +1,9 @@
+// Tone.pics Main JavaScript
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    console.log("Tone.pics loaded successfully");
+
+    // Future tools and features will be initialized here.
+
+});
